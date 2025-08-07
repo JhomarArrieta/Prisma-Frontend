cd984a612353929bfce3ea244079dc4c92058d86
María Agudelo <maria.aagudelo@udea.edu.co>
2025-08-07 09:56:26 -0500

    Refactor: Estructura auth - registro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LeftSide } from "./left-side/left-side";
-import { RightSide } from "./right-side/right-side";
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LeftSide } from "./core/auth/pages/registro/left-side/left-side";
+import { RightSide } from "./core/auth/pages/registro/right-side/right-side";
+import { Registro } from "./core/auth/pages/registro/registro";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LeftSide, RightSide],
+  imports: [RouterOutlet, RouterLink, LeftSide, RightSide, Registro],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/core/auth/pages/registro/registro.ts
+++ src/app/core/auth/pages/registro/registro.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { LeftSide } from "../left-side/left-side";
-import { RightSide } from "../right-side/right-side";
+import { LeftSide } from "./left-side/left-side";
+import { RightSide } from "./right-side/right-side";
 
 @Component({
   selector: 'app-registro',
