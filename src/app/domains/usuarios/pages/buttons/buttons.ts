db4ca1b09c804b828fcc37c08e746b780b1edd8a
sharaolaya <sharaolaya@gmail.com>
2025-08-08 09:30:10 -0500

    BugFixed: Componentes

--- src/app/domains/usuarios/pages/buttons/buttons.ts
+++ src/app/domains/usuarios/pages/buttons/buttons.ts
@@ -16,8 +16,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-action-buttons',
-  templateUrl: './action-buttons.component.html',
-  styleUrls: ['./action-buttons.component.css']
+  templateUrl: './buttons.html',
+  styleUrls: ['./buttons.css']
 })
 export class ActionButtonsComponent {
   
