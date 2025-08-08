91713638a52f77aa6f1985a32c95cd915c65910e
MariAgudelo2 <150271644+MariAgudelo2@users.noreply.github.com>
2025-08-08 11:57:57 -0500

    Feature: Rutas login - inicio

--- src/app/core/auth/pages/registro/auth-form-login/auth-form-login.ts
+++ src/app/core/auth/pages/registro/auth-form-login/auth-form-login.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { UsuariosApi } from '../../../../../domains/usuarios/services/usuarios-api';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-form-login',
@@ -13,7 +14,7 @@ import { CommonModule } from '@angular/common';
 export class AuthFormLogin {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder, private usuarioService: UsuariosApi) {
+  constructor(private fb: FormBuilder, private usuarioService: UsuariosApi, private router: Router) {
     this.form = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       contrasena: ['', [Validators.required, Validators.minLength(6)]]
@@ -26,6 +27,7 @@ export class AuthFormLogin {
       next: (res) => {
         console.log('Login exitoso', res);
         // Aquí puedes redirigir a otra página, guardar más cosas o actualizar UI
+        this.router.navigate(['/home']);
       },
       error: (err) => {
         console.log('Login fallido', err);
